1d9d5eb4590c024869b61e009ab4c74fffb6e197
McStecca <andreastecca2001@gmail.com>
2021-03-11 14:02:20 +0100

    merged two whiles

--- ex4.cpp
+++ ex4.cpp
@@ -7,19 +7,18 @@ int main(){
     cin >> C[i];
   }
 
-  int A[10];
+  int A[10], k = 0;
   for (int i = 0; i < 10; i++){//converts '(' in 1 and ')' in -1
     if (C[i]=='(') A[i]=1;
     else A[i]=-1;
-  }
-  int k = 0;
-  for (int i = 0; i < 10; i++){//sums and checks if it goes under 0
-    k += A[i];
+
+    k += A[i];//sums and checks if it goes under 0
     if (k<0){
       cout << "non bilanciato";
       return 0;
     }
   }
+  
   if(k>0)//checks if at the end it's balanced or not
     cout << "non bilanciato";
   else
