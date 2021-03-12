2979f692e3873f5c119486e7270beb0ebcb11189
McStecca <andreastecca2001@gmail.com>
2021-03-12 12:14:09 +0100

    comments

--- ex3.cpp
+++ ex3.cpp
@@ -7,9 +7,9 @@ int main(){
   for (int i = 0; i < 4; i++){//fills first array in input order
     cin >> A[i];
   }
-  for (int i = 3; i >= 0; i--){//fills second array in inverse input order
+  for (int i = 3; i >= 0; i--){//reads in inverse input order
     cin >> temp;
-    if(A[i] != temp){//checks if arrays are identical
+    if(A[i] != temp){//checks if input is identical
       ok = false; 
       break;
     }
